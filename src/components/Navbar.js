017b3bab893d186a58b9af05c18641fd027af1f1
Lenoms <90161737+Lenoms@users.noreply.github.com>
2022-04-13 12:21:58 +1000

    Add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+import DeleteIcon from '@mui/icons-material/Add'
+
+function Navbar() {
+  return (
+    <div>
+      <button><DeleteIcon/></button>
+    </div>
+  )
+}
+
+export default Navbar
