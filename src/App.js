a4227b4f9ca6afc510eecb38ca60195afacda862
Daniel-W-230 <dwat005@student.monash.edu>
2022-04-28 21:30:34 +1000

    File restructure and animations smoothed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import Header from "./components/Header";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import TriedList from "./components/TriedList";
-import UntriedList from "./components/UntriedList";
-import CreateCocktail from "./components/CreateCocktail";
+import TriedList from "./pages/TriedList";
+import UntriedList from "./pages/UntriedList";
+import CreateCocktail from "./pages/CreateCocktail";
 import { AnimatePresence } from "framer-motion";
-import CocktailInfo from "./components/CocktailInfo";
+import CocktailInfo from "./pages/CocktailInfo";
 
 function App() {
   let location = useLocation();
@@ -13,8 +13,8 @@ function App() {
     <div className="App">
       <Header />
 
-      <AnimatePresence exitBeforeEnter>
-        <Routes location={location}>
+      <AnimatePresence>
+        <Routes location={location} key={location.pathname}>
           <Route exact path="/cocktails/tried" element={<TriedList />} />
           <Route exact path="/cocktails/untried" element={<UntriedList />} />
           <Route exact path="/cocktails/create" element={<CreateCocktail />} />
