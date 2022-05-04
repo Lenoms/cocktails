e84dc4f9ea33203509a9ef91d8e313d78a940afd
Daniel-W-230 <dwat005@student.monash.edu>
2022-05-04 17:03:14 +1000

    Small UI changes

--- src/components/TriedCocktailItem.js
+++ src/components/TriedCocktailItem.js
@@ -4,6 +4,7 @@ import placeholder from "../placeholder.jpg";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { getDatabase, ref, remove } from "firebase/database";
 import { useNavigate } from "react-router-dom";
+import { Card } from "@mui/material";
 
 function TriedCocktailItem({ item }) {
   const navigate = useNavigate();
@@ -26,14 +27,14 @@ function TriedCocktailItem({ item }) {
     navigate("/cocktails/info", { state: { cocktailItem: item } });
   };
   return (
-    <div className="tried-cocktail-item-body" onClick={cocktailClicked}>
+    <Card className="tried-cocktail-item-body" onClick={cocktailClicked}>
       <h3 className="cocktail-name">{cocktailName}</h3>
       <h3>{cocktailGrade}</h3>
       <img id="cocktail-image" src={cocktailImageUrl}></img>
       <button className="delete-button" onClick={deleteCocktail}>
         <DeleteIcon />
       </button>
-    </div>
+    </Card>
   );
 }
 
