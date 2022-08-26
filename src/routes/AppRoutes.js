19d6c4825268033c2b3e1b5d5103297ea3b9d074
Daniel-W-230 <dwat005@student.monash.edu>
2022-08-26 21:09:06 +1000

    Small change to routes

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -10,45 +10,36 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 function AppRoutes({ sortBy, searchQuery }) {
   let location = useLocation();
-  const { isAuthenticated } = useAuth0();
-  if (isAuthenticated) {
-    return (
-      <Routes location={location} key={location.pathname}>
-        <Route
-          exact
-          path="/cocktails"
-          element={<TriedList sortBy={sortBy} searchQuery={searchQuery} />}
-        />
-        <Route
-          exact
-          path="/cocktails/tried"
-          element={<TriedList sortBy={sortBy} searchQuery={searchQuery} />}
-        />
-        <Route
-          exact
-          path="/cocktails/untried"
-          element={<UntriedList searchQuery={searchQuery} />}
-        />
-        <Route exact path="/cocktails/create" element={<CreateCocktail />} />
-        <Route
-          exact
-          path="/cocktails/info"
-          element={<CocktailInfo location={location} />}
-        />
-        <Route
-          exact
-          path="/cocktails/update"
-          element={<UpdateCocktailInfo location={location} />}
-        />
-      </Routes>
-    );
-  } else {
-    return (
-      <Routes>
-        <Route path="/*" element={<LoginPage />} />
-      </Routes>
-    );
-  }
+  return (
+    <Routes location={location} key={location.pathname}>
+      <Route
+        exact
+        path="/cocktails"
+        element={<TriedList sortBy={sortBy} searchQuery={searchQuery} />}
+      />
+      <Route
+        exact
+        path="/cocktails/tried"
+        element={<TriedList sortBy={sortBy} searchQuery={searchQuery} />}
+      />
+      <Route
+        exact
+        path="/cocktails/untried"
+        element={<UntriedList searchQuery={searchQuery} />}
+      />
+      <Route exact path="/cocktails/create" element={<CreateCocktail />} />
+      <Route
+        exact
+        path="/cocktails/info"
+        element={<CocktailInfo location={location} />}
+      />
+      <Route
+        exact
+        path="/cocktails/update"
+        element={<UpdateCocktailInfo location={location} />}
+      />
+    </Routes>
+  );
 }
 
 export default AppRoutes;
