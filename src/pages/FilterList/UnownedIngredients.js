b2b3b15c6426cbe13387a6dac1f13b9cc834f2e3
Daniel-W-230 <dwat005@student.monash.edu>
2022-09-25 17:05:08 +1000

    Update unowned ingredients list to look like a bar, kind of.

--- src/pages/FilterList/UnownedIngredients.js
+++ src/pages/FilterList/UnownedIngredients.js
@@ -2,13 +2,41 @@ import React from "react";
 import "./FilterList.css";
 
 function UnownedIngredients({ unownedIngredients }) {
+  const UnownedIngredient = (ingredient) => {
+    return (
+      <div className="unowned-ingredient-container">
+        {ingredient.ingredient}
+      </div>
+    );
+  };
+  const BarRow = (row) => {
+    return (
+      <div className="unowned-ingredients-bar-row">
+        {row.row.map((ingredient) => {
+          return <UnownedIngredient ingredient={ingredient.ingredient} />;
+        })}
+      </div>
+    );
+  };
+  const BarRows = () => {
+    let rows = splitArrayIntoChunksOfLen(unownedIngredients, 3);
+    return rows.map((row) => {
+      return <BarRow row={row} />;
+    });
+  };
+
+  function splitArrayIntoChunksOfLen(arr, len) {
+    var chunks = [],
+      i = 0,
+      n = arr.length;
+    while (i < n) {
+      chunks.push(arr.slice(i, (i += len)));
+    }
+    return chunks;
+  }
   return (
     <div className="unowned-ingredients-list-container">
-      <ul>
-        {unownedIngredients.map((ingredient) => {
-          return <li key={ingredient.ingredient}>{ingredient.ingredient}</li>;
-        })}
-      </ul>
+      <BarRows />
     </div>
   );
 }
