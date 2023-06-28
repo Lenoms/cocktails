41949df501b65223515ccfd33bb01d5639cf4aa5
Daniel-W-230 <dwat005@student.monash.edu>
2023-06-28 13:46:15 +1000

    Add herbaceous and original tags

--- src/components/Tags/Tag.constants.js
+++ src/components/Tags/Tag.constants.js
@@ -12,6 +12,10 @@ export const mapToColour = (option) => {
       return "#e6b0ff";
     case "aesthetic":
       return "#fcb083";
+    case "herbaceous":
+      return "#8BFFD7";
+    case "original":
+      return "#FFC0CB";
   }
   return "#8ff6f7";
 };
@@ -23,4 +27,10 @@ export const tagOptions = [
   { label: "strong", value: "strong", color: mapToColour("strong") },
   { label: "dessert", value: "dessert", color: mapToColour("dessert") },
   { label: "aesthetic", value: "aesthetic", color: mapToColour("aesthetic") },
+  {
+    label: "herbaceous",
+    value: "herbaceous",
+    color: mapToColour("herbaceous"),
+  },
+  { label: "original", value: "original", color: mapToColour("original") },
 ];
