bbca272ad387b8ba929cfd8f44ab117e07cd69d5
Daniel <danielwatson230@gmail.com>
2025-10-14 14:08:36 +1100

    Disabling auth0 temporarily, setting homepage to /cocktails

--- src/components/LogButtons/LogoutButton.js
+++ src/components/LogButtons/LogoutButton.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+//import { useAuth0 } from "@auth0/auth0-react";
 import "./LogButtons.css";
-import LogoutIcon from '@mui/icons-material/Logout';
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const LogoutButton = ({ size }) => {
-  const { logout } = useAuth0();
+  //const { logout } = useAuth0();
 
   return (
     <button
       className="log-button"
       style={{ width: size[0], height: size[1] }}
-      onClick={() =>
-        logout({ returnTo: window.location.origin + "/cocktails" })
-      }
+      // onClick={() =>
+      //   logout({ returnTo: window.location.origin + "/cocktails" })
+      // }
     >
-      <LogoutIcon style={{ color: "white"}}/>
+      <LogoutIcon style={{ color: "white" }} />
     </button>
   );
 };
